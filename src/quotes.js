a80bfecbde663eb9e81f2bad4c72de9f86921c64
Charlie Pye <charl@MacBook-Air.local>
2026-05-10 13:55:43 -0700

    initial quote voting app

---
+++ src/quotes.js
@@ -0,0 +1,40 @@
+export const BOOK_A = {
+  title: "Book One",
+  quotes: [
+    {
+      id: "a1",
+      text: "Do not let your fire go out, spark by irreplaceable spark in the hopeless swamps of the not-quite, the not-yet, and the not-at-all. Do not let the hero in your soul perish in lonely frustration for the life you deserved and have never been able to reach. The world you desire can be won. It exists.. it is real.. it is possible.. it's yours.",
+    },
+    {
+      id: "a2",
+      text: "I swear by my life and my love of it that I will never live for the sake of another man, nor ask another man to live for mine.",
+    },
+    {
+      id: "a3",
+      text: "I started my life with a single absolute: that the world was mine to shape in the image of my highest values and never to be given up to a lesser standard, no matter how long or hard the struggle.”",
+    },
+    {
+      id: "a4",
+      text: "If you saw Atlas, the giant who holds the world on his shoulders, if you saw that he stood, blood running down his chest, his knees buckling, his arms trembling but still trying to hold the world aloft with the last of his strength, and the greater his effort the heavier the world bore down upon his shoulders - What would you tell him?",
+    },
+  ],
+};
+
+export const BOOK_B = {
+  title: "Book Two", quotes: [
+    {
+      id: "b1",
+      text: "The year was 2081, and everybody was finally equal. They weren't only equal before God and the law. They were equal every which way. Nobody was smarter than anybody else. Nobody was better looking than anybody else. Nobody was stronger or quicker than anybody else. All this equality was due to the 211th, 212th, and 213th Amendments to the Constitution, and to the unceasing vigilance of agents of the United States Handicapper General.",
+    },
+    {
+      id: "b2",
+      text: "'I am the Emperor!' cried Harrison. 'Do you hear? I am the Emperor! Everybody must do what I say at once!' He stamped his foot and the studio shook. \n\n'Even as I stand here' he bellowed, 'crippled, hobbled, sickened - I am a greater ruler than any man who ever lived! Now watch me become what I can become !'",
+    },
+    {
+      id: "b3",
+      text: "Hazel had a perfectly average intelligence, which meant she couldn't think about anything except in short bursts. And George, while his intelligence was way above normal, had a little mental handicap radio in his ear. He was required by law to wear it at all times. It was tuned to a government transmitter. Every twenty seconds or so, the transmitter would send out some sharp noise to keep people like George from taking unfair advantage of their brains.",
+    },
+  ],
+};
+
+
