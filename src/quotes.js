268e2e4b3d579b758681fbb5c8e7450324d378db
Charlie Pye <charl@MacBook-Air.local>
2026-05-20 17:38:12 +0800

    added more quotes

--- src/quotes.js
+++ src/quotes.js
@@ -11,12 +11,36 @@ export const BOOK_A = {
     },
     {
       id: "a3",
-      text: "I started my life with a single absolute: that the world was mine to shape in the image of my highest values and never to be given up to a lesser standard, no matter how long or hard the struggle.”",
+      text: "I started my life with a single absolute: that the world was mine to shape in the image of my highest values and never to be given up to a lesser standard, no matter how long or hard the struggle.",
     },
     {
       id: "a4",
       text: "If you saw Atlas, the giant who holds the world on his shoulders, if you saw that he stood, blood running down his chest, his knees buckling, his arms trembling but still trying to hold the world aloft with the last of his strength, and the greater his effort the heavier the world bore down upon his shoulders - What would you tell him?",
     },
+    {
+      id: "a5",
+      text: "She sat listening to the music. It was a symphony of triumph. The notes flowed up, they spoke of rising and they were the rising itself, they were the essence and the form of upward motion, they seemed to embody every human act and thought that had ascent as its motive. It was a sunburst of sound, breaking out of hiding and spreading open. It had the freedom of release and the tension of purpose. It swept space clean, and left nothing but the joy of an unobstructed effort. Only a faint echo within the sounds spoke of that from which the music had escaped, but spoke in laughing astonishment at the discovery that there was no ugliness or pain, and there never had to be. It was the song of an immense deliverance.",
+    },
+    {
+      id: "a6",
+      text: "But there are people who'll try to hurt you through the good they see in you--knowing that it's the good, needing it and punishing you for it. Don't let it break you when you discover that.",
+    },
+    {
+      id: "a7",
+      text: "Happiness is a state of non-contradictory joy--a joy without penalty or guilt, a joy that does not clash with any of your values and does not work for your own destruction, not the joy of escaping from your mind, but of using your mind's fullest power, not the joy of faking reality, but of achieving values that are real, not the joy of a drunkard, but of a producer.",
+    },
+    {
+      id: "a8",
+      text: "Pride is the recognition of the fact that you are your own highest value and, like all of man’s values, it has to be earned. His own happiness is man’s only moral purpose, but only his own virtue can achieve it…Life is the reward of virtue- and happiness is the goal and the reward of life.",
+    },
+    {
+      id: "a9",
+      text: "Love is blind, they say; sex is impervious to reason and mocks the power of all philosophers. But, in fact, a man’s sexual choice is the result and the sum of his fundamental convictions. Tell me what a man finds sexually attractive and I will tell you his entire philosophy on life. Show me the woman he sleeps with and I will tell you his valuation of himself.",
+    },
+    {
+      id: "a10",
+      text: "We are on strike against martyrdom—and against the moral code that demands it. We are on strike against those who believe that one man must exist for the sake of another. We are on strike against the morality of cannibals, be it practiced in body or in spirit. We will not deal with men on any terms but ours—and our terms are a moral code which holds that man is an end in himself and not the means to any end of others.",
+    }
   ],
 };
 
@@ -34,7 +58,33 @@ export const BOOK_B = {
       id: "b3",
       text: "Hazel had a perfectly average intelligence, which meant she couldn't think about anything except in short bursts. And George, while his intelligence was way above normal, had a little mental handicap radio in his ear. He was required by law to wear it at all times. It was tuned to a government transmitter. Every twenty seconds or so, the transmitter would send out some sharp noise to keep people like George from taking unfair advantage of their brains.",
     },
+    {
+      id: "b4",
+      text: "He tried to think a little about the ballerinas. They weren't really very good-no better than anybody else would have been, anyway. They were burdened with sashweights and bags of birdshot, and their faces were masked, so that no one, seeing a free and graceful gesture or a pretty face, would feel like something the cat drug in. George was toying with the vague notion that maybe dancers shouldn't be handicapped. ",
+    },
+    {
+      id: "b5",
+      text: "'Ladies and Gentlemen,' said the ballerina, reading the bulletin. She must have been extraordinarily beautiful, because the mask she wore was hideous. And it was easy to see that she was the strongest and most graceful of all the dancers, for her handicap bags were as big as those worn by two-hundred pound men.",
+    },
+    {
+      id: "b6",
+      text: "It was then that Diana Moon Glampers, the Handicapper General, came into the studio with a double-barreled ten-gauge shotgun. She fired twice, and the Emperor and the Empress were dead before they hit the floor. Diana Moon Glampers loaded the gun again. She aimed it at the musicians and told them they had ten seconds to get their handicaps back on.",
+    },
+    {
+      id: "b7",
+      text: "'Harrison Bergeron, age fourteen,' she said in a grackle squawk, 'Has just escaped from jail, where he was held on suspicion of plotting to overthrow the government. He is a genius and an athlete, is under-handicapped, and should be regarded as extremely dangerous.'",
+    },
+    {
+      id: "b8",
+      text: "They weren't really very good-no better than anybody else would have been, anyway. They were burdened with sashweights and bags of birdshot, and their faces were masked, so that no one, seeing a free and graceful gesture or a pretty face, would feel like something the cat drug in. George was toying with the vague notion that maybe dancers shouldn't be handicapped. But he didn't get very far with it before another noise in his ear radio scattered his thoughts.",
+    },
+    {
+      id: "b9",
+      text: "Hazel had a perfectly average intelligence, which meant she couldn't think about anything except in short bursts. And George, while his intelligence was way above normal, had a little mental handicap radio in his ear. He was required by law to wear it at all times. It was tuned to a government transmitter. Every twenty seconds or so, the transmitter would send out some sharp noise to keep people like George from taking unfair advantage of their brains.",
+    },
+    {
+      id: "b10",
+      text: "And to offset Harrison's good looks, the H-G men required that he wear at all times a red rubber ball for a nose, keep his eyebrows shaved off, and cover his even white teeth with black caps at snaggle-tooth random.",
+    },
   ],
 };
-
-
